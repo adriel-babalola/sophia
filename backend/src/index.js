15189e7eafa8b4f61712c60a3956a258af58fa89
adriel-babalola <babalolaaauge115@st.futminna.edu.ng>
2026-08-02 19:51:28 +0100

    added login route, finished sign up route, both using token based authentication JWT, screte is in env, currently craeted the book schema and route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,23 @@ import express from "express";
 import "dotenv/config"
 
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
 import { connectDB } from "./lib/db.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json())
+
 app.use("/api/auth", authRoutes)
+app.use("/api/books", bookRoutes)
+
 
 app.listen(PORT, () => {     
     
     connectDB();
     console.log(`Server runnning fine on ${PORT}`);
     
- })
+ }) 
+ 
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,17 +1,99 @@
-import express from "express";
+import express, { request } from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
 
-router.post("/register", async (req, res) => { 
-    try {
-      
-    } catch (error) {
-      console.log("Error", error)
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
- })
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 caharacters long " });
+    }
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 3 char long " });
+    }
 
- router.post("/login", async (req, res) => { 
-    res.send("login")
-  })
+    // if user already exsist
+    const exsistingEmail = await User.findOne({ email });
+    if (exsistingEmail) {
+      return res.status(400).json({ message: "Email already exsist" });
+    }
 
-export default router
+    const exsistingUsername = await User.findOne({ username });
+    if (exsistingUsername) {
+      return res.status(400).json({ message: "Username already exsist" });
+    }
+
+    // getting a random avatar
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    const user = new User({
+      email,
+      username,
+      password,
+      profileImage,
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in register route", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    // If user already exsists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentails" });
+
+    // If password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentails" });
+
+    // generating the token
+    const token = generateToken(user._id);
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+export default router;
