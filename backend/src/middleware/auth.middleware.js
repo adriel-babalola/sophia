8196e99f182bed89abc98463c719177eebf76fa1
adriel-babalola <babalolaaauge115@st.futminna.edu.ng>
2026-08-06 20:11:15 +0100

    completed book routes with protect route auth middleware, route can post, get and delete books, used cloudinary for cloud storage for the images, remeber we are only storing the cloudinary image url in mongoDB

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js"
+import "dotenv/config"
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer ", "")
+        if (!token) return res.status(401).json({ message : "No authentication token, access denied"})
+
+        // verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        // find the user 
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if (!user) {
+            return res.status(401).json({message: "Token is not valid"})
+        }
+
+        req.user = user;
+
+        next()
+    } catch (error) {
+        console.log("Authetication error:", error.message)
+        res.status(401).json({ message: "Token is not valid "})
+    }
+}
+
+export default protectRoute;
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,5 +1,104 @@
-import express from "express"
+import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
+import protectRoute from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-export default router
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ message: "Please provide all fields " });
+    }
+
+    // uploading image to clodinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    // save to db
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Eroor Creating Book", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error in get all books route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // deleting the image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from cloudinary", deleteError);
+      }
+    }
+    await book.deleteOne();
+
+    res.json({ message: "Book deleted succesfully" });
+  } catch (error) {
+    console.log("Error deleting book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.json(books);
+  } catch (error) {
+    console.error("Get user books error: ", error.message);
+    res.status(500).json({ message: "Server error " });
+  }
+});
+
+export default router;
